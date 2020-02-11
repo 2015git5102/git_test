fa62a33177c8c619d8d19ff66c42e509f9201bc8
maotai <maotai@itcast.cn>
2020-02-11 22:32:20 +0800

    修改内容

--- github/src/com/itheima/Git.java
+++ github/src/com/itheima/Git.java
@@ -6,4 +6,9 @@ public class Git {
         System.out.println("11111111111");
         System.out.println("11111111111");
     }
+    public void test(){
+        System.out.println("新增部分");
+        System.out.println("新增部分");
+        System.out.println("新增部分");
+    }
 }
