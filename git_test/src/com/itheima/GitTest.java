a589402e7974c26a60868a01eaf703cd4a5ff519
maotai <maotai@itcast.cn>
2020-02-11 20:49:41 +0800

    把工程添加到git

---
+++ git_test/src/com/itheima/GitTest.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git ..............");
+        System.out.println("Hello Git ..............");
+        System.out.println("Hello Git ..............");
+        System.out.println("Hello Git ..............");
+        System.out.println("Hello Git ..............");
+        System.out.println("Hello Git ..............");
+    }
+}
